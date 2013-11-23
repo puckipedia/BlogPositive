211da9e61cba733ac733a2ba3f2c6a35ad5a8a9f
Puck Meerburg <puck@puckipedia.nl>
2013-11-23 20:26:29 +0000

    Fixed style for BlogPositivePluginLoader
    * May not compile
    * Also, may not even process

--- API/BlogPositivePluginLoader.cpp
+++ API/BlogPositivePluginLoader.cpp
@@ -1,4 +1,89 @@
 #include "BlogPositivePluginLoader.h"
 
-BList *BlogPositivePluginLoader::List;
-CreateWindowPlugin *BlogPositivePluginLoader::Plugin;
+#include "BlogPositiveBlog.h"
+#include "BlogPositivePlugin.h"
+#include "../BlogPositivePlugins/CreateWindowPlugin/CreateWindowPlugin.h"
+#include "../Example/ExamplePlugin.h"
+#include "BlogPositivePluginWindow.h"
+#include <List.h>
+
+
+BList *BlogPositivePluginLoader::fList;
+CreateWindowPlugin *BlogPositivePluginLoader::fPlugin;
+
+void
+BlogPositivePluginLoader::Initialize()
+{
+    List = new BList();
+    Plugin = new CreateWindowPlugin();
+    List->AddItem(Plugin);
+    List->AddItem(new ExamplePlugin());
+}
+
+void
+BlogPositivePluginLoader::LoadWindow(BlogPositiveBlog *aBlog)
+{
+    BlogPositivePlugin *aWorkingPlugin = 0;
+    for(int i = 0; i < fList->CountItems(); i++) {
+	BlogPositivePlugin *aPlugin = (BlogPositivePlugin *)fList->ItemAt(i);
+	if(aPlugin->Type() == kBlogPositiveBlogApi &&
+	   aPlugin->Supports(aBlog->GetBlogHandler())) {
+	    aWorkingPlugin = aPlugin;
+	    break;
+	}
+    }
+    if(aWorkingPlugin == 0)
+	return;
+
+    BlogPositivePluginPostListWindow *aBlogWindow = 
+	new BlogPositivePluginPostListWindow(BRect(100, 100, 600, 700), aBlog->GetName());
+
+    blog->SetPlugin(aWorkingPlugin);
+    aBlogWindow->SetBlog(aBlog);
+
+    for(int i = 0; i < fList->CountItems(); i++) {
+	BlogPositivePlugin *plugin = (BlogPositivePlugin *)fList->ItemAt(i);
+	if(plugin->Type() == kBlogPositiveBlogEditor) {
+	    plugin->HookBlogList(&aBlogWindow, aBlog);
+	}
+    }
+
+    aBlogWindow->Show();
+}
+
+void
+BlogPositivePluginLoader::OpenPostWindow(BlogPositivePost *aPost)
+{
+    BlogPositivePluginBlogPostWindow *aBlogWindow = 
+	new BlogPositivePluginBlogPostWindow(BRect(100, 100, 600, 700), post->Name());
+    
+    aBlogWindow->SetPost(post);
+    
+    for(int i = 0; i < fList->CountItems(); i++) {
+	BlogPositivePlugin *plugin = (BlogPositivePlugin *)List->ItemAt(i);
+	if(plugin->Type() == kBlogPositiveBlogEditor) {
+	    plugin->HookEditor(&aBlogWindow, aPost);
+	}
+    }
+
+    aBlogWindow->Show();
+}
+
+void
+BlogPositivePluginLoader::OpenPostWindowQueryingPlugins(BWindow *aWindow)
+{
+    for(int i = 0; i < fList->CountItems(); i++) {
+	BlogPositivePlugin *aPlugin = (BlogPositivePlugin *)fList->ItemAt(i);
+	if(aPlugin->Type() == kBlogPositiveBlogEditor) {
+	    BlogPositivePost *aPost = aPlugin->TryGetPost(aWindow);
+	    if(aPost != NULL) {
+		OpenPostWindow(aPost);
+		return;
+	    }
+	}
+    }
+}
+const char *
+BlogPositivePluginLoader::GetPageContent(BlogPositivePluginBlogPostWindow *aWindow) {
+    return fPlugin->GetPostContent(aWindow);
+}
--- API/BlogPositivePluginLoader.h
+++ API/BlogPositivePluginLoader.h
@@ -1,79 +1,22 @@
-#pragma once
-#include "BlogPositiveBlog.h"
-#include "BlogPositivePlugin.h"
-#include <List.h>
-#include "../BlogPositivePlugins/CreateWindowPlugin/CreateWindowPlugin.h"
-#include "../Example/ExamplePlugin.h"
-#include "BlogPositivePluginWindow.h"
-#include <stdio.h>
+#ifndef BP_PLUGIN_LOADER_H
+#define BP_PLUGIN_LOADER_H
+
+class BWindow;
+class BList;
+class BlogPositiveBlog;
+class BlogPositivePost;
+class BlogPositivePluginBlogPostWindow;
+class CreateWindowPlugin;
 class BlogPositivePluginLoader {
 public:
-  static BList *List;
-  static CreateWindowPlugin *Plugin;
- public:
-  inline static void Initialize() {
-    List = new BList();
-    Plugin = new CreateWindowPlugin();
-    List->AddItem(Plugin);
-    List->AddItem(new ExamplePlugin());
-  }	
-  inline static void LoadWindow(BlogPositiveBlog *blog) {
-    BlogPositivePlugin *working = 0;
-    int i = 0;
-    for(; i < List->CountItems(); i++) {
-      printf("Trying plugin %d:",i);
-      BlogPositivePlugin *plugin = (BlogPositivePlugin *)List->ItemAt(i);
-      if(plugin->Type() == kBlogPositiveBlogApi && plugin->Supports(blog->GetBlogHandler())) {
-	working = plugin;
-	printf(" :D\n");
-	break;
-      } else {
-	printf(" D:\n");
-      }
-    }
-    if(working == 0)
-      return;
-    BlogPositivePluginPostListWindow *blogWindow = new BlogPositivePluginPostListWindow(BRect(100, 100, 600, 700), blog->GetName());
-    blog->SetPlugin(working);
-    blogWindow->SetBlog(blog);
-    for(i = 0; i < List->CountItems(); i++) {
-      BlogPositivePlugin *plugin = (BlogPositivePlugin *)List->ItemAt(i);
-      if(plugin->Type() == kBlogPositiveBlogEditor) {
-	plugin->HookBlogList(&blogWindow, blog);
-      }
-    }
-    if(blogWindow == 0) {
-      return;
-    }
-    printf("Running %d now\n", blogWindow);
-    blogWindow->Run();
-    blogWindow->Show();
-  }
-  inline static void OpenPostWindow(BlogPositivePost *post) {
-    BlogPositivePluginBlogPostWindow *blogWindow = new BlogPositivePluginBlogPostWindow(BRect(100, 100, 600, 700), post->Name());
-    blogWindow->SetPost(post);
-    for(int i = 0; i < List->CountItems(); i++) {
-      BlogPositivePlugin *plugin = (BlogPositivePlugin *)List->ItemAt(i);
-      if(plugin->Type() == kBlogPositiveBlogEditor) {
-	plugin->HookEditor(&blogWindow, post);
-      }
-    }
-    blogWindow->Run();
-    blogWindow->Show();
-  }
-  inline static void OpenPostWindowQueryingPlugins(BWindow *window) {
-    for(int i = 0; i < List->CountItems(); i++) {
-      BlogPositivePlugin *plugin = (BlogPositivePlugin *)List->ItemAt(i);
-      if(plugin->Type() == kBlogPositiveBlogEditor) {
-	BlogPositivePost *post = plugin->TryGetPost(window);
-	if(post != NULL) {
-	  OpenPostWindow(post);
-	  return;
-	}
-      }
-    }
-  }
-  inline static const char *GetPageContent(BlogPositivePluginBlogPostWindow *window) {
-    return Plugin->GetPostContent(window);
-  }
+    static void Initialize();
+    static void LoadWindow(BlogPositiveBlog *blog);
+    static void OpenPostWindow(BlogPositivePost *post);
+    static void OpenPostWindowQueryingPlugins(BWindow *window);
+    static const char *GetPageContent(BlogPositivePluginBlogPostWindow *window);
+private:
+    static BList *fList;
+    static CreateWindowPlugin *fPlugin;
 };
+
+#endif
