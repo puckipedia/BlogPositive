c6c68b8da738cbfe16ce50d6575a86ce938c0329
Puck Meerburg <puck@puckipedia.nl>
2013-11-23 20:34:59 +0000

    Fixed BlogPositivePluginWindow.{cpp,h}
    * Doesn't build!
    * Also, I like bacon

--- API/BlogPositivePluginWindow.cpp
+++ API/BlogPositivePluginWindow.cpp
@@ -1,49 +1,72 @@
 #include "BlogPositivePluginWindow.h"
+
+#include <Window.h>
+#include <Message.h>
+
+#include "BlogPositiveBlog.h"
 #include "BlogPositivePluginLoader.h"
-#include <stdio.h>
+#include "BlogPositivePost.h"
+
+
+BlogPositivePluginPostListWindow(BRect rect, const char *Name)
+    : BWindow(rect, Name, B_DOCUMENT_WINDOW, 0)
+{
+    
+}
 
 void
-BlogPositivePluginPostListWindow::SetBlog(BlogPositiveBlog *blog) {
-  fBlog = blog;
-  printf("%d\n", blog);
+BlogPositivePluginPostListWindow::SetBlog(BlogPositiveBlog *blog)
+{
+    fBlog = blog;
 }
 
 BlogPositiveBlog *
-BlogPositivePluginPostListWindow::Blog() {
-  return fBlog;
+BlogPositivePluginPostListWindow::Blog()
+{
+    return fBlog;
 }
 
 void
-BlogPositivePluginPostListWindow::MessageReceived(BMessage *message) {
-  switch(message->what) {
-  case kPostWindowGetSelection:
-    BlogPositivePluginLoader::OpenPostWindowQueryingPlugins(this);
-    break;
-  default:
-    BWindow::MessageReceived(message);
-  }
+BlogPositivePluginPostListWindow::MessageReceived(BMessage *message)
+{
+    switch(message->what)
+    {
+    case kPostWindowGetSelection:
+	BlogPositivePluginLoader::OpenPostWindowQueryingPlugins(this);
+	break;
+    default:
+	BWindow::MessageReceived(message);
+    }
+}
+
+BlogPositivePluginBlogPostWindow(BRect rect, const char *Name)
+ : BWindow(rect, Name, B_DOCUMENT_WINDOW, 0)
+{
+    
 }
 
 void
-BlogPositivePluginBlogPostWindow::SetPost(BlogPositivePost *post) {
-  fPost = post;
+BlogPositivePluginBlogPostWindow::SetPost(BlogPositivePost *post)
+{
+    fPost = post;
 }
-
 
 BlogPositivePost *
-BlogPositivePluginBlogPostWindow::Post() {
-  return fPost;
+BlogPositivePluginBlogPostWindow::Post()
+{
+    return fPost;
 }
 
-
 void
-BlogPositivePluginBlogPostWindow::MessageReceived(BMessage *message) {
-  switch(message->what) {
-  case kPostWindowSavePost:
-    Post()->SetPage(BlogPositivePluginLoader::GetPageContent(this));
-    Post()->Blog()->GetPlugin()->SavePost(fPost);
-    break;
-  default:
-    BWindow::MessageReceived(message);
-  }
+BlogPositivePluginBlogPostWindow::MessageReceived(BMessage *message)
+{
+    switch(message->what)
+    {
+    case kPostWindowSavePost:
+	Post()->SetPage(BlogPositivePluginLoader::GetPageContent(this));
+	Post()->Blog()->GetPlugin()->SavePost(fPost);
+	break;
+    default:
+	BWindow::MessageReceived(message);
+    }
 }
--- API/BlogPositivePluginWindow.h
+++ API/BlogPositivePluginWindow.h
@@ -1,26 +1,32 @@
-#pragma once
+#ifndef BP_PLUGIN_WINDOW_H
+#define BP_PLUGIN_WINDOW_H
+
 #include <Window.h>
 #include "BlogPositiveBlog.h"
 #include <Message.h>
 #include "BlogPositivePost.h"
+
 const uint32 kPostWindowGetSelection = 'PWgs';
 const uint32 kPostWindowSavePost = 'PWsp';
 const uint32 kPostWindowNewPost = 'PWnp';
+
 class BlogPositivePluginPostListWindow : public BWindow {
  public:
-  BlogPositivePluginPostListWindow(BRect rect, const char *Name) : BWindow(rect, Name, B_DOCUMENT_WINDOW, 0) {}
-  void SetBlog(BlogPositiveBlog *blog);
-  BlogPositiveBlog *Blog();
-  void MessageReceived(BMessage *msg);
- private:
-  BlogPositiveBlog *fBlog;
+    BlogPositivePluginPostListWindow(BRect rect, const char *Name) : BWindow(rect, Name, B_DOCUMENT_WINDOW, 0) {}
+    void SetBlog(BlogPositiveBlog *blog);
+    BlogPositiveBlog *Blog();
+    void MessageReceived(BMessage *msg);
+private:
+    BlogPositiveBlog *fBlog;
 };
 class BlogPositivePluginBlogPostWindow : public BWindow {
- public:
-  BlogPositivePluginBlogPostWindow(BRect rect, const char *Name) : BWindow(rect, Name, B_DOCUMENT_WINDOW, 0) {}
-  void SetPost(BlogPositivePost *post);
-  BlogPositivePost *Post();
-  void MessageReceived(BMessage *msg);
- private:
-  BlogPositivePost *fPost;
+public:
+    BlogPositivePluginBlogPostWindow(BRect rect, const char *Name) : BWindow(rect, Name, B_DOCUMENT_WINDOW, 0) {}
+    void SetPost(BlogPositivePost *post);
+    BlogPositivePost *Post();
+    void MessageReceived(BMessage *msg);
+private:
+    BlogPositivePost *fPost;
 };
+
+#endif
