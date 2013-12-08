617be7de77f1c9ef1c17a3e7684b67b108c04d50
Puck Meerburg <puck@puckipedia.nl>
2013-12-08 12:23:20 +0000

    Fix CreateWindowPlugin style

--- BlogPositivePlugins/CreateWindowPlugin/CreateWindowPlugin.cpp
+++ BlogPositivePlugins/CreateWindowPlugin/CreateWindowPlugin.cpp
@@ -19,107 +19,131 @@
 
 class PostItem : public BStringItem {
 public:
-  inline PostItem(BlogPositivePost *post) : BStringItem(post->Name()) {
-    fPost = post;
-  }
-  inline BlogPositivePost *Post() {
-    return fPost;
-  }
- private:
-  BlogPositivePost *fPost;
+						PostItem(BlogPositivePost* post);
+	BlogPositivePost*	Post();
+private:
+	BlogPositivePost*	fPost;
 };
 
+
+PostItem::PostItem(BlogPositivePost* post)
+	:
+	BStringItem(post->Name())
+{
+	fPost = post;
+}
+
+
+BlogPositivePost*
+PostItem::Post()
+{
+	return fPost;
+}
+
+
 class PluginAndWindowThing {
- public:
-  BWindow *fWindow;
-  BlogPositivePlugin *fPlugin;
-  BlogPositiveBlog *fBlog;
-  BListView *fView;
+public:
+	BWindow* fWindow;
+	BlogPositivePlugin* fPlugin;
+	BlogPositiveBlog* fBlog;
+	BListView* fView;
 };
 
 
 uint32
 CreateWindowPlugin::Version()
 {
-    return 0;
+	return 0;
 }
+
 
 int32
 CreateWindowPlugin::Type()
 {
-    return kBlogPositiveBlogEditor;
+	return kBlogPositiveBlogEditor;
 }
+
 
 void
-CreateWindowPlugin::HookBlogList(BlogPositivePluginPostListWindow **aWindow, BlogPositiveBlog *aBlog)
+CreateWindowPlugin::HookBlogList(BlogPositivePluginPostListWindow** aWindow,
+	BlogPositiveBlog* aBlog)
 {
-    BWindow *win = *aWindow;
-    BListView *aView = new BListView("ListView");
-    BMessage *msg = new BMessage(kPostWindowGetSelection);
-    aView->SetInvocationMessage(msg);
-    win->SetLayout(new BGroupLayout(B_VERTICAL));
-    win->AddChild(BGroupLayoutBuilder(B_VERTICAL, 10).Add(aView));
-    PluginAndWindowThing *thing = new PluginAndWindowThing();
-    thing->fWindow = *aWindow;
-    thing->fBlog = aBlog;	
-    thing->fView = aView;
-    thing->fPlugin = aBlog->Plugin();
-    thread_id readThread = spawn_thread(loadList, "load_posts", B_NORMAL_PRIORITY, thing);
-    resume_thread(readThread);
+	BWindow* win = *aWindow;
+	BListView* aView = new BListView("ListView");
+	BMessage* msg = new BMessage(kPostWindowGetSelection);
+	aView->SetInvocationMessage(msg);
+	win->SetLayout(new BGroupLayout(B_VERTICAL));
+	win->AddChild(BGroupLayoutBuilder(B_VERTICAL, 10).Add(aView));
+	PluginAndWindowThing* thing = new PluginAndWindowThing();
+	thing->fWindow = *aWindow;
+	thing->fBlog = aBlog;
+	thing->fView = aView;
+	thing->fPlugin = aBlog->Plugin();
+	thread_id readThread = spawn_thread(loadList, "load_posts",
+		B_NORMAL_PRIORITY, thing);
+	resume_thread(readThread);
 }
 
-BlogPositivePost *
-CreateWindowPlugin::TryGetPost(BWindow *aWindow)
+
+BlogPositivePost*
+CreateWindowPlugin::TryGetPost(BWindow* aWindow)
 {
-    BListView *aView = (BListView *)aWindow->FindView("ListView");
-    if(aView != NULL) {
-	int32 ChosenOne = aView->CurrentSelection();
-	if(ChosenOne == -1) {
+	BListView* aView = (BListView*)aWindow->FindView("ListView");
+	if (aView != NULL) {
+		int32 ChosenOne = aView->CurrentSelection();
+		if (ChosenOne == -1) {
+			return NULL;
+		}
+		PostItem* item = (PostItem*)aView->ItemAt(ChosenOne);
+		return item->Post();
+	}
 	return NULL;
-      }
-      PostItem *item = (PostItem *)aView->ItemAt(ChosenOne);
-      return item->Post();
-    }
 }
+
 
 int32
-CreateWindowPlugin::loadList(void *t)
+CreateWindowPlugin::loadList(void* t)
 {
-    PluginAndWindowThing *thing = (PluginAndWindowThing *)t;
-    BList *list = thing->fPlugin->GetBlogPosts(thing->fBlog);
-    if(thing->fView->LockLooper()) {
-      for(int i = 0; i < list->CountItems(); i++) {
-	BlogPositivePost *post = (BlogPositivePost *)list->ItemAt(i);
-	thing->fView->AddItem(new PostItem(post));
-      }
-      thing->fView->UnlockLooper();
-    }
+	PluginAndWindowThing* thing = (PluginAndWindowThing*)t;
+	BList* list = thing->fPlugin->GetBlogPosts(thing->fBlog);
+	if (thing->fView->LockLooper()) {
+		for (int i = 0; i < list->CountItems(); i++) {
+			BlogPositivePost* post = (BlogPositivePost*)list->ItemAt(i);
+			thing->fView->AddItem(new PostItem(post));
+		}
+		thing->fView->UnlockLooper();
+	}
 }
+
 
 void
-CreateWindowPlugin::HookEditor(BlogPositivePluginBlogPostWindow **aWindow, BlogPositivePost *aPost)
+CreateWindowPlugin::HookEditor(BlogPositivePluginBlogPostWindow**aWindow,
+	BlogPositivePost* aPost)
 {
-    BlogPositivePluginBlogPostWindow *win = *aWindow;
+	BlogPositivePluginBlogPostWindow* win = *aWindow;
 
-    BTextView *aView = new BTextView("TextView");
-    aView->SetText(aPost->Page());
+	BTextView* aView = new BTextView("TextView");
+	aView->SetText(aPost->Page());
 
-    BMenuBar *menuBar = new BMenuBar("MenuBar");
+	BMenuBar* menuBar = new BMenuBar("MenuBar");
 
-    win->SetLayout(new BGroupLayout(B_VERTICAL));
-    win->AddChild(BGroupLayoutBuilder(B_VERTICAL, 0).Add(menuBar).Add(aView));
+	win->SetLayout(new BGroupLayout(B_VERTICAL));
+	win->AddChild(menuBar);
+	win->AddChild(aView);
 
-    BMenu *menu = new BMenu("File");
+	BMenu* menu = new BMenu("File");
 
-    menu->AddItem(new BMenuItem("Save", new BMessage(kPostWindowSavePost), 'S'));
+	menu->AddItem(new BMenuItem("Save",
+		new BMessage(kPostWindowSavePost), 'S'));
 
-    menuBar->AddItem(menu);
+	menuBar->AddItem(menu);
 }
 
-const char *
-CreateWindowPlugin::GetPostContent(BlogPositivePluginBlogPostWindow *aWindow)
+
+const char*
+CreateWindowPlugin::GetPostContent(BlogPositivePluginBlogPostWindow* aWindow)
 {
-    BView *aView = aWindow->FindView("TextView");
-    BTextView *aTextView = (BTextView *)aView;
-    return aTextView->Text();
+BView* aView = aWindow->FindView("TextView");
+BTextView* aTextView = (BTextView*)aView;
+return aTextView->Text();
 }
--- BlogPositivePlugins/CreateWindowPlugin/CreateWindowPlugin.h
+++ BlogPositivePlugins/CreateWindowPlugin/CreateWindowPlugin.h
@@ -2,9 +2,9 @@
  * Copyright 2013 Puck Meerburg, puck@puckipedia.nl
  * All rights reserved. Distributed under the terms of the MIT License.
  */
-
 #ifndef BP_CREATE_WINDOW_PLUGIN_H
 #define BP_CREATE_WINDOW_PLUGIN_H
+
 
 #include "../../API/BlogPositivePlugin.h"
 
@@ -16,13 +16,15 @@ class BlogPostiveBlog;
 class BWindow;
 class CreateWindowPlugin : public BlogPositivePlugin {
 public:
-    uint32 Version();
-    int32 Type();
-    void HookBlogList(BlogPositivePluginPostListWindow **window, BlogPositiveBlog *blog);
-    BlogPositivePost *TryGetPost(BWindow *window);
-    static int32 loadList(void *t);
-    void HookEditor(BlogPositivePluginBlogPostWindow **window, BlogPositivePost *post);
-    const char *GetPostContent(BlogPositivePluginBlogPostWindow *window);
+	uint32				Version();
+	int32				Type();
+	void				HookBlogList(BlogPositivePluginPostListWindow** window,
+							BlogPositiveBlog* blog);
+	BlogPositivePost*	TryGetPost(BWindow* window);
+	static int32		loadList(void* t);
+	void				HookEditor(BlogPositivePluginBlogPostWindow** window,
+							BlogPositivePost* post);
+	const char*			GetPostContent(BlogPositivePluginBlogPostWindow* window);
 };
 
 #endif
